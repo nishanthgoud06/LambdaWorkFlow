e0e236eb7e3882e3ec7887692591b5faf4cb9813
nishanth.goud06 <nishanthgoud1999@gmail.com>
2025-01-01 14:04:25 -0600

    added sns construct to get email notification

---
+++ infra/lib/SNSConstruct.ts
@@ -0,0 +1,31 @@
+import { Construct } from 'constructs';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { EmailSubscription, SqsSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+
+interface SNSConstructProps {
+    emailAddress: string;
+}
+
+export class SNSConstruct extends Construct {
+    readonly topic: Topic;
+    readonly queue: Queue;
+
+    constructor(scope: Construct, id: string, props: SNSConstructProps) {
+    super(scope, id);
+
+    // Create an SNS Topic
+    this.topic = new Topic(this, 'S3FileAddedTopic', {
+        displayName: 'Notification for new S3 objects',
+    });
+
+    // Create an SQS Queue
+    this.queue = new Queue(this, 'S3EventsQueue');
+
+    // Subscribe SQS Queue to the SNS Topic
+    this.topic.addSubscription(new SqsSubscription(this.queue));
+
+    // Subscribe an email address to the SNS Topic
+    this.topic.addSubscription(new EmailSubscription(props.emailAddress));
+    }
+}
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,19 +1,35 @@
-import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { CfnOutput, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket, EventType } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import { LambdaBConstruct } from './LambdaBConstruct';
 import { LambdaAConstruct } from './LambdaAConstruct';
+import { SNSConstruct } from './SNSConstruct';
+import { SnsDestination } from 'aws-cdk-lib/aws-s3-notifications';
 
 export class InfraStack extends Stack {
+  readonly bucket: Bucket;
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-      const bucket = new Bucket(this, 'DataBucket', {
+      this.bucket = new Bucket(this, 'DataBucket', {
       versioned: true,
-      removalPolicy: RemovalPolicy.DESTROY
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
     });
 
+    const snsConstruct = new SNSConstruct(this, 'SNSConstruct', {
+      emailAddress: 'gidovi2161@matmayer.com'
+    });
+
+    this.bucket.addEventNotification(
+      EventType.OBJECT_CREATED,
+      new SnsDestination(snsConstruct.topic),
+      {
+        suffix: '.json',
+      }
+    );
+
     const lambdaB = new LambdaBConstruct(this, 'LambdaB', {
-      bucketName: bucket
+      bucketName: this.bucket
     });
 
     const lambdaA = new LambdaAConstruct(this, 'LambdaA', {
@@ -21,5 +37,9 @@ export class InfraStack extends Stack {
     });
 
     lambdaB.lambdaFunction.grantInvoke(lambdaA.lambdaFunction.role!);
+    
+    new CfnOutput(this, 'ConfirmEmail', {
+      value: 'Check your email and confirm the subscription to start receiving notifications.',
+    });
   }
 }
